17cba66311481389affd27c6a4b1d465021ceee6
ted2020 <tuncay2020@gmail.com>
2022-05-22 17:12:13 -0500

    primitive types 2

--- primitive_types.sol
+++ primitive_types.sol
@@ -8,6 +8,9 @@ contract MyContract {
     uint256 public myUint256 = 1; 
     uint8 public myUint8 = 1;
     address public myAdress = 0x7875aeF2932cc14b144f5836C046C2B3a44c2ABD;
+    bool public myBool = true;
+
+
 
 
 
