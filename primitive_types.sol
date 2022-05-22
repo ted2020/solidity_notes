327873c0f929a5cd33de5b72db37e6187b45d3fd
ted2020 <tuncay2020@gmail.com>
2022-05-22 17:10:44 -0500

    primitive types

---
+++ primitive_types.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.0;
+
+contract MyContract {
+    string public myString = "Hello Class";
+    bytes32 public myBytes32 = "hello again class";
+    int public myInt = 1;
+    uint public myUint = 1;
+    uint256 public myUint256 = 1; 
+    uint8 public myUint8 = 1;
+    address public myAdress = 0x7875aeF2932cc14b144f5836C046C2B3a44c2ABD;
+
+
+
+}
